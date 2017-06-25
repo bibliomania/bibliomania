43dbd20c1c2b5e88eca3f65aedf891009513e77d
seri <seri.wb@gmail.com>
2017-06-26 04:40:11 +0900

    create template project

---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
