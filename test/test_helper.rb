a881d71ba41ac689974a674ad51882cd6c33b754
seri <seri.wb@gmail.com>
2017-06-27 20:25:23 +0900

    add test settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,3 +20,17 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+module BetterAssertResponse
+  def assert_response(*args)
+    super
+  rescue Minitest::Assertion => e
+    if response.body.size < 200
+      better_message = "#{e.message}\nResponse body: #{response.body}"
+      raise Minitest::Assertion, better_message
+    else
+      raise
+    end
+  end
+end
+ActionDispatch::IntegrationTest.prepend(BetterAssertResponse)
